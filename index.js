6da34e9becc91d938ad98b498699f79069ffa2a4
Cobancho <tomycastelli@gmail.com>
2023-10-18 19:48:23 -0300

    Changed to port 3001

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { getFiles, getFileDownload } = require('./s3')
 require('dotenv').config()
 
 const app = express()
-const port = 3002
+const port = 3001
 
 app.listen(port, () => {
 	console.log(`Server is running on http://localhost:${port}`)
